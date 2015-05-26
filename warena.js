c4d63667ce240cbecc7d51e95919efea22cd5773
romaintailhurat <romain.tailhurat@gmail.com>
2015-05-26 21:43:13 +0200

    Creation of a an hexagon in the canvas

--- warena.js
+++ warena.js
@@ -2,4 +2,23 @@
 
 function createGridAtCanvas(id) {
   var canvas = document.getElementById(id);
+  var context = canvas.getContext('2d');
+
+  var numberOfSides = 6,
+      size = 40,
+      Xcenter = 100,
+      Ycenter = 100;
+
+  context.beginPath();
+  context.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));
+
+  for (var i = 1; i <= numberOfSides;i += 1) {
+      context.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+  }
+
+  context.strokeStyle = "#000000";
+  context.lineWidth = 1;
+  context.stroke();
 }
+
+createGridAtCanvas('hex');
